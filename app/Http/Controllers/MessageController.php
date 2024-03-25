2bb20811d9caa76ac63375a267f55aeea0207f00
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-03-25 10:04:10 +0900

    MessageController#indexアクションの実装

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MessageController extends Controller
 {
-    //
+    // indexアクション
+    public function index(): View
+    {
+        $messages = Message::all();
+        // messagesとういうキーでビューに渡す
+        return view('message/index', [
+            'messages' => $messages
+        ]);
+    }
 }
