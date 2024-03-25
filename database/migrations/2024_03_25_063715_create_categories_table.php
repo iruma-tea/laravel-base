48843ae4bb68d951ce1a0f9a2e8052304b45e568
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-03-25 15:55:41 +0900

    categories,booksマイグレートファイルの編集、マイグレートの実行

--- database/migrations/2024_03_25_063715_create_categories_table.php
+++ database/migrations/2024_03_25_063715_create_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
             $table->timestamps();
         });
     }
--- database/migrations/2024_03_25_063726_create_books_table.php
+++ database/migrations/2024_03_25_063726_create_books_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->string('author', 50);
+            $table->integer('price');
+            $table->foreignId('category_id')->constrained();
             $table->timestamps();
         });
     }
