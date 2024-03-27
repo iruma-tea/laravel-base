c465fcbd16cf4a7d845bbef2e45f07b5ac786889
iruma-tea <shusuke.think.pad@gmail.com>
2024-03-27 14:45:17 +0900

    prefixを使用したグループ化の定義に更新

--- routes/web.php
+++ routes/web.php
@@ -22,5 +22,9 @@ Route::get('/', function () {
 Route::get('messages', [MessageController::class, 'index']);
 Route::post('messages', [MessageController::class, 'store']);
 
-Route::get('admin/books', [BookController::class, 'index'])->name('book.index');
-Route::get('admin/books/{id}', [BookController::class, 'show'])->whereNumber('id')->name('book.show');
+// Route::get('admin/books', [BookController::class, 'index'])->name('book.index');
+// Route::get('admin/books/{id}', [BookController::class, 'show'])->whereNumber('id')->name('book.show');
+Route::prefix('admin/books')->group(function () {
+    Route::get('', [BookController::class, 'index'])->name('book.index');
+    Route::get('{id}', [BookController::class, 'show'])->whereNumber('id')->name('book.show');
+});
