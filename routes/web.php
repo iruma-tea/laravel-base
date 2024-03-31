63226d0306e43b01add3ac53300c2b67a56931f4
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-03-31 15:23:00 +0900

    ルーティング設定をマージ

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\BookController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,4 +30,17 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
+
+
+Route::get('messages', [MessageController::class, 'index']);
+Route::post('messages', [MessageController::class, 'store']);
+Route::prefix('admin/books')->name('book.')->controller(BookController::class)->group(function () {
+    Route::get('', 'index')->name('index');
+    Route::get('{book}', 'show')->whereNumber('book')->name('show');
+    Route::get('create', 'create')->name('create');
+    Route::post('', 'store')->name('store');
+    Route::get('{book}/edit', 'edit')->whereNumber('book')->name('edit');
+    Route::put('{book}', 'update')->whereNumber('book')->name('update');
+    Route::delete('{book}', 'destroy')->whereNumber('book')->name('destroy');
+});
