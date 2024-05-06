ec8fdc5fedd808bb4a63e665cd4a57e36660c62a
shusuke-iruma-tea <shusuke.pana@gmail.com>
2024-05-06 17:49:00 +0900

    Chap08 エラーハンドリングとロギングの学習完了

--- config/app.php
+++ config/app.php
@@ -159,6 +159,7 @@ return [
         /*
          * Package Service Providers...
          */
+        App\Providers\DatabaseServiceProvider::class,
 
         /*
          * Application Service Providers...
