2cf48c56a55c01d03f21f68c0d6575cb9cb681f4
iruma-tea <shusuke.think.pad@gmail.com>
2024-05-17 17:28:17 +0900

    フィーチャーテストの学習開始

---
+++ tests/Feature/MessageTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Message;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class MessageTest extends TestCase
+{
+    use RefreshDatabase;
+    /**
+     * A basic feature test example.
+     */
+    // public function test_example(): void
+    // {
+    //     $response = $this->get('/');
+
+    //     $response->assertStatus(200);
+    // }
+
+    /** @test */
+    public function メッセージ一覧の表示(): void
+    {
+        // メッセージ一覧にリクエストを送信
+        // $response = $this->get('messages');
+        // // 200(OK)が返る。
+        // $response->assertOk();
+
+        // 事前情報としてメッセージ作成
+        Message::create(['body' => 'Hello World']);
+        Message::create(['body' => 'Hello Laravel']);
+
+        print_r(Message::all());
+
+        // メッセージ一覧にHello World、Hello Laravelが表示される
+        $this->get('messages')->assertOk()->assertSee('Hello World')->assertSee('Hello Laravel');
+    }
+}
